70a17cdc38138c1f8f63ce593b00167342c0b33d
akthernahela <nahela00@gmail.com>
2025-11-04 18:15:52 +0100

    Header almost done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,39 +4,43 @@ export default function Header() {
 
     return (
         <header>
-            <ul className="nav">
+            <div>
                 <Logo />
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">CHARACTERS</a>
-                </li>
-                <li className="nav-item nav-underline">
-                    <a className="nav-link active" aria-current="page" href="#">COMICS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">MOVIES</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">TV</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">GAMES</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">VIDEOS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">FANS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">NEWS</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">SHOP</a>
-                </li>
-            </ul>
+            </div>
+            <div>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+                    </li>
+                    <li className="nav-item nav-underline">
+                        <a className="nav-link active" aria-current="page" href="#">COMICS</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">MOVIES</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">TV</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">GAMES</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">VIDEOS</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">FANS</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">NEWS</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">SHOP</a>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
