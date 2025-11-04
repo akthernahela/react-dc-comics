ea0b1f2b6af8c4308fd1a45972ff6d08f634052b
akthernahela <nahela00@gmail.com>
2025-11-04 18:09:25 +0100

    Give the center position to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,37 +4,37 @@ export default function Header() {
 
     return (
         <header>
-            <Logo />
-            <ul class="nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+            <ul className="nav">
+                <Logo />
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">CHARACTERS</a>
                 </li>
-                <li class="nav-item nav-underline">
-                    <a class="nav-link active" aria-current="page" href="#">COMICS</a>
+                <li className="nav-item nav-underline">
+                    <a className="nav-link active" aria-current="page" href="#">COMICS</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">MOVIES</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">MOVIES</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">TV</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">TV</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">GAMES</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">GAMES</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">COLLECTIBLES</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">VIDEOS</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">VIDEOS</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">FANS</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">FANS</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">NEWS</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">NEWS</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">SHOP</a>
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">SHOP</a>
                 </li>
             </ul>
         </header>
