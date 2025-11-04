35bde71990539c2ffb5a76f3ceb9d90e3f1c4741
akthernahela <nahela00@gmail.com>
2025-11-04 23:05:09 +0100

    Try to fix the bug

--- src/components/BigLogo.jsx
+++ src/components/BigLogo.jsx
@@ -1,5 +1,5 @@
 import BigOne from '../assets/img/dc-logo-bg.png';
 export default function BigLogo() {
 
-    return <img className='BigLogo' width="450" src={BigOne} alt="" />
+    return <img className='BigLogo' width="550" src={BigOne} alt="" />
 }
