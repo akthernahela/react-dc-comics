15d9d4ac6025266da75bc89208a3f81c802e915d
akthernahela <nahela00@gmail.com>
2025-11-06 12:13:38 +0100

    Copy the main jsx col to the comiccard

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,6 +1,13 @@
 export default function ComicCard(props) {
 
     return (
-
-    );
+        <div className="col" key={comic.id}>
+            <div className="card h-100">
+                <img className="card-img-top" src={comic.thumb} alt='' />
+                <div className="card-body">
+                    <div>{comic.series}</div>
+                </div>
+            </div>
+        </div>
+    )
 }    
