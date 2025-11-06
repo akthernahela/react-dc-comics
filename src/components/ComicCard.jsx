912b1458ff313ba8475ce0e71159112b61559825
akthernahela <nahela00@gmail.com>
2025-11-06 12:20:43 +0100

    Create a variable

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,11 +1,11 @@
 export default function ComicCard(props) {
-
+    const { comic } = props;
     return (
-        <div className="col" key={comic.id}>
+        <div className="col">
             <div className="card h-100">
-                <img className="card-img-top" src={comic.thumb} alt='' />
+                <img className="card-img-top" src={thumb} alt='' />
                 <div className="card-body">
-                    <div>{comic.series}</div>
+                    <div>{series}</div>
                 </div>
             </div>
         </div>
